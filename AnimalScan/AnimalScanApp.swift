820c269cd643190c62ff978ca6b55a4715fa5b22
Jules <julesliegeois@gmail.com>
2026-06-01 14:20:25 +0200

    Initial Commit

---
+++ AnimalScan/AnimalScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalScanApp.swift
+//  AnimalScan
+//
+//  Created by Jules Liegeois on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct AnimalScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
