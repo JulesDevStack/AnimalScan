c366f5e85faf11da461ecd979a3b034ba7a6ceac
Yamine3 <Yamine3>
2026-06-04 19:48:56 +0200

    Ajout de détails dans la page 'SheetList' presque terminé : Image qui peuvent posé problemes selon leur cadrage à corriger

--- AnimalScan/Views/SheetView/SheetListView.swift
+++ AnimalScan/Views/SheetView/SheetListView.swift
@@ -10,18 +10,44 @@ import SwiftUI
 struct SheetListView: View {
     var body: some View {
         NavigationStack {
-            List(sheets){ sheet in
-                NavigationLink{
-                    FicheView(sheet: sheet)
-                }label: {
-                    HStack{
-                        Image(sheet.image)
-                            .resizable()
-                            .scaledToFill()
-                        Text(sheet.name)
+            ZStack{
+                LinearGradient(gradient: Gradient(colors: [Color.background1, Color.background2, Color.background3]), startPoint: .topLeading, endPoint: .bottom)
+                    .ignoresSafeArea()
+                VStack{
+                    
+                    Text("Vos cartes")
+                        .font(.title)
+                        .padding(.top, 20)
+                    
+                    List(sheets){ sheet in
+                        NavigationLink{
+                            FicheView(sheet: sheet)
+                        }label: {
+                            HStack{
+                                Image(sheet.image)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 80, height: 80)
+                                    .clipShape(RoundedRectangle(cornerRadius: 32))
+                                
+                                Text(sheet.name)
+                                    .font(.headline)
+                            }
+                        }
+                        .listRowBackground(
+                            RoundedRectangle(cornerRadius: 16)
+                                .fill(.ultraThinMaterial)
+                                
+                        )
                     }
+                    .listRowSpacing(10)
+
+                    .scrollContentBackground(.hidden)
                 }
+               
+                
             }
+            
         }
     }
 }
